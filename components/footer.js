cf81c303166addd1ab019d8f55106b107d46054e
Plamen Karolev <asenov78@gmail.com>
2026-08-05 17:19:02 +0300

    Simplify footer to just the project name

--- components/footer.js
+++ components/footer.js
@@ -2,12 +2,8 @@
 export default function Footer() {
     return (
         <footer className="text-center items-center">
-
-            <div className="mt-5">
-            </div>
-            <div className="mt-5">
-                <a href="https://github.com/lswiderski/WebBodyComposition" target="_blank" rel="noopener noreferrer" className="pr-5 underline">GitHub</a>
-                <a href="https://twitter.com/L_swiderski" target="_blank" rel="noopener noreferrer" className="pl-5 underline">Twitter</a>
+            <div className="mt-5 mb-3 text-gray-500 text-sm">
+                web-body-composition
             </div>
         </footer>
     )
